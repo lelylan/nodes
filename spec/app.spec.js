c1f0cbe5597258a415fd6a2200900e773563e804
Andrea Reginato <andrea.reginato@gmail.com>
2013-06-28 17:38:21 +0200

    Working tests for the setting topic case

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,13 +1,15 @@
 var request = require('supertest')
   , app = require('../app')
-  , ascoltatori = require("ascoltatori")
-  , async = require("async")
-  , chai = require("chai")
-  , expect = require("chai").expect
-  , sinon = require("sinon")
-  , sinonChai = require("sinon-chai")
+  , ascoltatori = require('ascoltatori')
+  , async = require('async')
+  , chai = require('chai')
+  , expect = require('chai').expect
+  , sinon = require('sinon')
   , debug = require('debug')('test');
-chai.use(sinonChai);
+
+chai.use(require('sinon-chai'));
+chai.use(require('chai-fuzzy'));
+
 
 var execute
   , ascoltatore
@@ -17,6 +19,7 @@ var execute
       db: process.env.MONGOLAB_JOBS_DB,
       pubsubCollection: 'mqtt',
       mongo: {} };
+
 
 describe('MQTT node',function() {
 
@@ -31,24 +34,29 @@ describe('MQTT node',function() {
         .put('/mqtt/devices/device-1/properties/set')
         .set('Content-Type', 'application/json')
         .set('X-Physical-Secret', 'secret-1')
-        .send({ id: 'device-1', properties: [{ id: 'property-1', value: 'on'}] })
+        .send({id: 'device-1', properties: [{ id: 'property-1', value: 'on'}]})
     });
 
     it('returns 202', function(done) {
       execute.expect(202).expect({ status: 202 }, done);
     });
 
-    //it('publish on /mqtt/:secret/set', function(done) {
-      //async.series([
-        //function(cb) {
-          //ascoltatori.build(settings, function(ascoltatore) {
-            //ascoltatore.subscribe('mqtt/secret-1/set', done); cb();
-          //});
-        //},
-        //function(cb) {
-          //execute.expect(202, cb)
-        //}
-      //]);
-    //});
+    it('publish on /mqtt/:secret/set', function(done) {
+      async.series([
+        function(cb) {
+          ascoltatori.build(settings, function(ascoltatore) {
+            ascoltatore.subscribe('mqtt/secret-1/set', function() {
+              expect(arguments['0']).to.be.equal('mqtt/secret-1/set');
+              expect(arguments['1']).to.be.like({id: 'device-1', properties: [{ id: 'property-1', value: 'on'}]});
+              done()
+            });
+            cb();
+          });
+        },
+        function(cb) {
+          execute.expect(202, cb)
+        }
+      ]);
+    });
   });
 });
